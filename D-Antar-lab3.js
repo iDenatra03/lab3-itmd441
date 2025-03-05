c814cc061149b91c2c2345fc3ba8c996200b96e9
iDenatra03 <dantar@hawk.iit.edu>
2025-03-05 00:35:23 -0600

    Update D-Antar-lab3.js

--- D-Antar-lab3.js
+++ D-Antar-lab3.js
@@ -1,3 +1,11 @@
 // Denatra Antar
 //ITMD 441-02 Undergraduate Student
 
+//Excersise 1
+
+function minMaxAverage(numbers) {
+    var totalNumbers = numbers.length;
+    var minValue = numbers[0];
+    var maxValue = numbers[0];
+    var sum = 0;
+}
